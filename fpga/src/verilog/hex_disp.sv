97b6ec8120769a76e8596064c4adb4883ed81710
jdudik6 <jdudik6@gatech.edu>
2025-02-09 19:16:52 -0500

    Added hex display module.

---
+++ fpga/src/verilog/hex_disp.sv
@@ -0,0 +1,49 @@
+module hex_disp
+(
+    input logic [3:0] hex_val,
+    input logic cs,
+    input logic free,
+    input logic resetn,
+    
+    output logic [6:0] segments
+);
+
+    logic [3:0] latched_hex;
+    logic [3:0] hex_d;
+
+    always @(posedge cs, resetn) begin
+        if (resetn == '0)
+            latched_hex <= 4'b0000; 
+        else begin
+            latched_hex <= hex_val;
+        end
+    end
+
+    always_comb begin
+        case(free) 
+            '0: hex_d = latched_hex;
+            '1: hex_d = hex_val;
+        endcase
+
+        case(hex_d)
+            4'b0000: segments = "1000000";
+            4'b0001: segments = "1111001";
+            4'b0010: segments = "0100100";
+            4'b0011: segments = "0110000";
+            4'b0100: segments = "0011001";
+            4'b0101: segments = "0010010";
+            4'b0110: segments = "0000010";
+            4'b0111: segments = "1111000";
+            4'b1000: segments = "0000000";
+            4'b1001: segments = "0010000";
+            4'b1010: segments = "0001000";
+            4'b1011: segments = "0000011";
+            4'b1100: segments = "1000110";
+            4'b1101: segments = "0100001";
+            4'b1110: segments = "0000110";
+            4'b1111: segments = "0001110";
+            default: segments = "0111111";
+        endcase
+    end
+
+endmodule
