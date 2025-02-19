fe17a76bebb9a2cb29e045b11e144c79f4829dd3
Ian Crombie <irowdy04@gmail.com>
2025-02-18 23:08:39 -0500

    update readme

--- src/software/dataset_generation.py
+++ src/software/dataset_generation.py
@@ -32,7 +32,7 @@ parser.add_argument('--time', type=float, default=1, help="Define the time perio
 parser.add_argument('-v', '--verbose', action='store_true', default=False, help="Verbose output to console.")
 parser.add_argument('--noise_samples', type=int, default=1, help="Define number of noise samples to overlay on each digit.")
 parser.add_argument('--samples_per_dft', type=int, default=256, help="Number of samples used for each DFT.")
-parser.add_argument('--time_min', type=float, default=0.8, help="The minimum time length for input files, files below this are zero-padded.")
+parser.add_argument('--time_min', type=float, default=1, help="The minimum time length for input files, files below this are zero-padded.")
 args = parser.parse_args()
 
 # TODO: Maybe parameterize the number of speakers to generate spectrograms for? 
