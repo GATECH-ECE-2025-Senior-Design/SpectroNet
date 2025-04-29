ba562241e29327ce30ee47061bd6b48b0033b7d4
Tejasvi Yadavalli <143543526+Padmamithra@users.noreply.github.com>
2025-04-29 17:06:52 -0400

    Update test_librosa.py

--- sw/software/test_librosa.py
+++ sw/software/test_librosa.py
@@ -5,9 +5,9 @@ file_path = "/Users/padmamithra/Downloads/SpectroNet/sw/datasets/digits_train/7_
 
 try:
     y, sr = librosa.load(file_path, sr=16000)
-    print(f"✅ Successfully loaded: {file_path} (Length: {len(y)} samples, Sample Rate: {sr})")
+    print(f"Successfully loaded: {file_path} (Length: {len(y)} samples, Sample Rate: {sr})")
 except Exception as e:
-    print(f"❌ Error loading file: {e}")
+    print(f"Error loading file: {e}")
 
 if __name__ == "__main__":
     multiprocessing.set_start_method("spawn", force=True)
